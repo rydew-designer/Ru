4fb103fcca6d0d9ed1bc0c57c01241fd272d81c2
rydew-designer <dmitryrydnew@gmail.com>
2026-05-07 01:34:51 +0300

    Add signing config for release APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,15 @@ android {
         versionName = "1.0"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("release.jks")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -22,6 +31,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
